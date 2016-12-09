45ea9536f5f0c36a6a6d0f28bc9831ed67ed1f23
Supasate Choochaisri <supasate.c@gmail.com>
2016-12-09 22:44:00 +0700

    A minimal react repo with react-hot-loader version 3

---
+++ minimal/src/index.js
@@ -0,0 +1,22 @@
+import { AppContainer } from 'react-hot-loader'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const renderWithHotReload = (MainComponent) => {
+  ReactDOM.render(
+    <AppContainer>
+       <MainComponent />
+    </AppContainer>,
+    document.getElementById('react-root')
+  )
+}
+
+renderWithHotReload(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    renderWithHotReload(NextApp)
+  })
+}
